a04dcea8070523801f67667f79630b3705f2ebb1
Tiffany Chen <tiffanychen@Tiffanys-MacBook-Pro.local>
2018-09-30 21:13:36 -0400

    added comments and code cleanup

--- homework_5/checked.js
+++
@@ -1,8 +0,0 @@
-checked();
-
-function checked() {
-    if(document.querySelector('input[name="size"]:checked') == true){
-        var size = document.querySelector('input[name="size"]:checked').id;
-        size.style.fontSize = "150%";
-    }
-}
